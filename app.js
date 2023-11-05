70a83aed42de4c06e02429cb036c79c8e427c132
Shrayash Shukla <ishrayash@gmail.com>
2023-11-05 07:07:23 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,134 @@
+import express from "express";
+import passport from "passport";
+import GitHubStrategy from "passport-github2";
+import session from "express-session";
+import fetch from "node-fetch";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const app = express();
+const port = process.env.PORT || 3000;
+const __dirname = dirname(fileURLToPath(import.meta.url));
+// Configure session middleware
+app.use(
+  session({
+    secret: "c00b8d5dfe1ad4c26b05265073d777dd6637451c",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(express.static(__dirname + "/views")); //to add css files
+
+// GitHub OAuth 2.0 strategy configuration
+passport.use(
+  new GitHubStrategy(
+    {
+      clientID: "b1967e670e6e4183b0f1", // Replace with your GitHub OAuth App's Client ID
+      clientSecret: "c00b8d5dfe1ad4c26b05265073d777dd6637451c", // Replace with your GitHub OAuth App's Client Secret
+      callbackURL: "http://localhost:3000/auth/github/callback",
+    },
+    (accessToken, refreshToken, profile, done) => {
+      // Store user profile information or perform other actions as needed
+      return done(null, { profile, accessToken });
+    }
+  )
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
+
+// Set EJS as the view engine
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+
+// Define routes
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+// Redirect to GitHub for authentication when the button is clicked
+app.get("/auth/github", passport.authenticate("github"));
+
+// GitHub callback route after authentication
+app.get(
+  "/auth/github/callback",
+  passport.authenticate("github", { failureRedirect: "/" }),
+  (req, res) => {
+    // Successful authentication, retrieve user data and latest commits here
+    const { profile, accessToken } = req.user;
+
+    // Fetch user data
+    fetch("https://api.github.com/user", {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    })
+      .then((response) => response.json())
+      .then((userData) => {
+        // Fetch latest commits
+        fetch(`https://api.github.com/user/repos?per_page=5`, {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        })
+          .then((response) => response.json())
+          .then((repositories) => {
+            // // Render the data in the 'index' view
+            // console.log(userData);
+            // console.log(repositories);
+            // res.render("index", { user: userData, repositories });
+            // Fetch the latest commits for each repository
+            const promises = repositories.map((repo) => {
+              return fetch(
+                `https://api.github.com/repos/${repo.full_name}/commits`,
+                {
+                  headers: {
+                    Authorization: `Bearer ${accessToken}`,
+                  },
+                }
+              )
+                .then((response) => response.json())
+                .then((commits) => {
+                  repo.commits = commits; // Add the commits to the repository data
+                });
+            });
+
+            // Wait for all promises to resolve
+            Promise.all(promises)
+              .then(() => {
+                // Render the data in the 'index' view
+                // console.log(userData);
+                // console.log(repositories);
+
+                console.log(userData);
+                console.log(repositories);
+                res.json({ user: userData, repo: repositories });
+              })
+              .catch((error) => {
+                console.error("Error fetching commits:", error);
+                res.status(500).send("Internal server error");
+              });
+          })
+          .catch((error) => {
+            console.error("Error fetching repositories:", error);
+            res.status(500).send("Internal server error");
+          });
+      })
+      .catch((error) => {
+        console.error("Error fetching user data:", error);
+        res.status(500).send("Internal server error");
+      });
+  }
+);
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
